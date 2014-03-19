61bedbcde66aa00f97ea258878634b772185f1c4
Francis Tseng <ftzeng@gmail.com>
2014-03-19 00:06:15 -0400

    begun porting from salt => ansible

--- deploy/pillar/settings.sls
+++
@@ -1,9 +0,0 @@
-# Setup Git info.
-git_rev: master
-git_repo: git@github.com:publicscience/argos.git
-
-# Application info.
-{% set app_name = 'argos' %}
-app_name: {{ app_name }}
-app_path: /var/www/{{ app_name }}/
-env_path: /var/env/
--- deploy/salt/scripts/install-rabbitmq.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo 'deb http://www.rabbitmq.com/debian testing main' | sudo tee -a /etc/apt/sources.list
-wget http://www.rabbitmq.com/rabbitmq-signing-key-public.asc
-sudo apt-key add rabbitmq-signing-key-public.asc
-rm rabbitmq-signing-key-public.asc
-
-sudo apt-get update -y
