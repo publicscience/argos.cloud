176f10900d25b5cea5fb5b678ec7599518cac059
Francis Tseng <ftzeng@gmail.com>
2014-04-01 12:15:42 -0400

    hashbang for ponder

--- playbooks/roles/collector/templates/ponder.sh
+++ playbooks/roles/collector/templates/ponder.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+
+PIDFILE=/tmp/ponder.pid
+
+if [ -e "${PIDFILE}" ]; then
+  echo "Already running."
+  exit 99
+fi
+
+# Ensure PID file is removed on program exit.
+trap "rm -f -- '$PIDFILE'" EXIT
+
+# Create a file with current PID to indicate that process is running.
+echo $! > "$PIDFILE"
+chmod 644 "$PIDFILE"
+
 cd {{ app_path }}
 {{ venv_path }}/bin/python -c 'from argos.core.membrane.collector import ponder; ponder()'
 
