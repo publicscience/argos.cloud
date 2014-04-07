14667b33047ff6d60544aebba09fde9c995244a5
Francis Tseng <ftzeng@gmail.com>
2014-04-07 10:22:36 -0400

    broker and worker roles beefed up, collector (for now) takes on both

--- playbooks/roles/app/templates/celery_config.py
+++ playbooks/roles/app/templates/celery_config.py
@@ -9,7 +9,7 @@ BROKER_CONNECTION_MAX_RETRIES = None
 CELERY_RESULT_BACKEND = 'redis://{{ broker_host }}:6379/0'
 
 # What modules to import on start.
-CELERY_IMPORTS = ('argos.tasks',)
+CELERY_IMPORTS = ('argos.tasks', 'argos.tasks.periodic', )
 
 # Propagate chord errors when they come up.
 CELERY_CHORD_PROPAGATES = True
@@ -22,14 +22,15 @@ CELERY_MAX_CACHED_RESULTS = 1
 
 # Send emails on errors
 CELERY_SEND_TASK_ERROR_EMAILS = True
-ADMINS = (
+ADMINS = [
     ('{{ celery_admin_name }}', '{{ celery_admin_email }}')
-)
+]
 SERVER_EMAIL = '{{ celery_email_user }}'
 EMAIL_HOST = '{{ email_host }}'
-EMAIL_PORT = 587
+EMAIL_PORT = {{ email_port }}
 EMAIL_HOST_USER = '{{ celery_email_user }}'
 EMAIL_HOST_PASSWORD = '{{ celery_email_password }}'
+EMAIL_USE_TLS = True
 
 # If enabled pid and log directories will be created if missing.
 CELERY_CREATE_DIRS=1
@@ -40,3 +41,39 @@ CELERY_CREATE_DIRS=1
 # approach, but see:
 # https://github.com/publicscience/argos/issues/112
 CELERYD_MAX_TASKS_PER_CHILD=100
+
+
+from celery.schedules import crontab
+
+CELERY_TIMEZONE = 'UTC'
+CELERYBEAT_SCHEDULE = {
+    #'collect-articles': {
+        #'task': 'argos.tasks.periodic.collect',
+        #'schedule': crontab(minute=0, hour='*')
+    #},
+    #'cluster-articles': {
+        #'task': 'argos.tasks.periodic.cluster_articles',
+        #'schedule': crontab(minute=30, hour='*')
+    #},
+    #'cluster-events': {
+        #'task': 'argos.tasks.periodic.cluster_events',
+        #'schedule': crontab(minute=30, hour='*')
+    #},
+    'test-task': {
+        'task': 'argos.tasks.notify',
+        'schedule': crontab(minute=30, hour='*'),
+        'args': ('this is a beat test',)
+    }
+}
+
+from kombu.common import Broadcast, Queue
+
+# Create a broadcast queue so the tasks are sent to
+# *all* workers (that are listening to that queue).
+CELERY_DEFAULT_QUEUE = 'default'
+CELERY_QUEUES = (Queue('default'), Broadcast('broadcast_tasks'), )
+CELERY_ROUTES = {
+        'argos.tasks.periodic.collect': {'queue': 'broadcast_tasks'},
+        'argos.tasks.periodic.cluster_articles': {'queue': 'broadcast_tasks'},
+        'argos.tasks.periodic.cluster_events': {'queue': 'broadcast_tasks'}
+}
